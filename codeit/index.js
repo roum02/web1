c9b9b8a63e564a766981d0066237536bec7b91cb
roum02 <78200124+roum02@users.noreply.github.com>
2021-02-21 23:31:39 +0900

    Add files via upload

---
+++ codeit/index.js
@@ -0,0 +1,14 @@
+console.log('Hellow Codeit!'); 
+console.log(15); //주석 comment
+/* 콘솔 탭에 Hellow Codeit 로그를 추가해줘 */
+
+// 변수선언
+let espressoPrice = 3000;
+let lattePrice = 4300;
+let mochaPrice = 4000;
+
+console.log(espressoPrice * 2);
+console.log(lattePrice* 4 + mochaPrice * 2);
+console.log(lattePrice + mochaPrice);
+console.log(espressoPrice + lattePrice *4);
+console.log(lattePrice * 3);
---
+++ codeit/index2.js
@@ -0,0 +1,21 @@
+//함수 선언
+function greeting(sentence){
+  console.log('Hi');
+  console.log('안녕');
+  console.log(sentence);
+}
+
+//함수 호출
+greeting('Hola');
+
+function welcome(name){
+  console.log('안녕하세요 '+name+ '님!');
+}
+
+welcome('코드잇');
+
+function printSquare(x){
+  console.log(x*x);
+}
+
+printSquare(2);
