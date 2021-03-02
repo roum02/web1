75b711fd5176cf1afd44b5279227a082b427e3e2
roum02 <78200124+roum02@users.noreply.github.com>
2021-03-02 22:08:50 +0900

    Add files via upload

--- codeit/index2.js
+++ codeit/index2.js
@@ -23,3 +23,14 @@ printSquare(2);
 //숫자형
 
 console.log(1+8);
+
+//불린형
+console.log(3 >= 2); //등호를 항상 = 앞에 써주어야 한다.
+console.log(3 === 3); // ===는 일치한다는 기호
+console.log(3 !== 3);
+
+//앤드 연산자 &&
+//오어 연산자 ||
+//낫 연산자 !
+console.log(!ture);
+console.log(!!ture); //true의 반대의 반대
