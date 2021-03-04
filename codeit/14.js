9879456ac1dd1ae3df22667d9828212d170acf70
roum02 <78200124+roum02@users.noreply.github.com>
2021-03-04 23:36:03 +0900

    Add files via upload

---
+++ codeit/14.js
@@ -0,0 +1,29 @@
+//형 변환
+//숫자 -> 문자
+/*
+let x = '123';
+console.log(x);
+console.log(Number(x));
+console.log(String(x));
+console.log(typeof x);
+console.log(typeof String(x));
+*/
+console.log('');
+
+//불린 -> 문자
+let y = false;
+console.log(y);
+console.log(Number(y)); // 0과 1의 값으로 변함
+console.log(String(y));
+console.log(typeof y);
+console.log(typeof String(y));
+
+
+console.log('');
+
+//문자 -> 불린
+let x = '문자';
+console.log(x);
+console.log(Boolean(x));
+console.log(typeof x);
+console.log(typeof Boolean(x));
