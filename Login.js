b8659c501159b592b185f868f80b5ebe62247e2e
roum02 <78200124+roum02@users.noreply.github.com>
2021-07-11 15:02:56 +0900

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Input from "../components/Input";
+
+const Login = () => {
+  return (
+    <div>
+      로그인
+      <Input name="이름"></Input>
+    </div>
+  );
+};
+
+export default Login;
